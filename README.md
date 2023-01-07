9bfcfd74ee897d9a198302d51cf157ca435412a5
Lucas Nguyen <lucasoAlt@outlook.com>
2023-01-07 18:44:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fake_bsod for my gf
+Copy of https://github.com/LucasoDevDotTk/Fake_bsod, with modified BSOD text for my beloved girlfriend. Thanks for everything, Isabella. Love you!
+___
+
+## HOW TO
+
+1. Download Python from python.org.
+2. Press on the file to times, make sure you are in Windows 10 or any other Windows operating system.
+____
+
+## LICENSE
+MIT License
+
+```
+All code in this file is licensed under the MIT License, the whole license and copyright holder(s) is in ./LICENSE.
+
+You shall follow all the terms of the MIT License (./LICENSE).
+```
+___
+
+## Contact
+You can contct me by sending me an email at lucasoAlt@outlook.com.
+
+## Thanks so much for downloading our software 😀🎉
+
+### Please report issues to the GitHub Page of [this project.](https://github.com/LucasoDevDotTk/Fake_bsod)
+
+<code style="color:green;">Happy coding!</code>
+
+
---
+++ main.py
@@ -0,0 +1,42 @@
+"""
+All code in this file is licensed under the MIT License, the whole license and copyright holder(s) is in ./LICENSE.
+
+You shall follow all the terms of the MIT License (./LICENSE).
+"""
+
+# Using: keyboard == 0.13.5 by Lucas Boppre Niehues (https://github.com/boppreh/keyboard)
+
+import subprocess
+import os
+from random import randint
+from time import sleep
+import sys
+
+if sys.platform == "win32":
+    os.chdir(os.path.dirname(__file__))
+
+    pip_file_path = str(f"{os.path.dirname(__file__)}/bat/pip.bat")
+    subprocess.call([pip_file_path])
+
+    try:
+        import keyboard  # pip install keyboard
+        range_repeat = randint(1, 3)
+        for i in range(range_repeat):
+            print("ERROR CANT FIND CURRENT WORKING DIRECTORY")
+            keyboard.press_and_release("F11")
+            keyboard.press_and_release("F11")
+            keyboard.press_and_release("F11")
+            keyboard.press_and_release("F11")
+            keyboard.press_and_release("F11")
+            sleep(0.01)
+            keyboard.press_and_release("F11")
+            keyboard.press_and_release("F11")
+
+        bat_file_path = str(f"{os.path.dirname(__file__)}/bat/bsod.bat")
+        subprocess.call([bat_file_path])
+
+    except:
+        input("Sorry something went wrong in the installation process. Please turn on internet and try again.")
+
+else:
+    input("Sorry, this program is only compatible with Windows.")
